d1598c3342ad600e43b530e63c33e67e56de7776
gonela0526 <gonela.sumanth@gmail.com>
2021-08-29 18:31:17 +0530

    Express&mongosetup

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
-
 const app = express();
+const connectDB = require('./config/db');
+
+//DB Connection 
+connectDB();
 
 app.get('/',(re,res) => res.send("API is running"))
+
+//Define Routes
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/posts',require('./routes/api/posts'));
+app.use('/api/profile',require('./routes/api/profile'));
 
 const PORT  = process.env.PORT || 5000;
 
